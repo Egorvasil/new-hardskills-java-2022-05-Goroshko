f60507e601db2a3e5bfee478d48174d81a4d2213
mrGostem <106623766+mrGostem@users.noreply.github.com>
2022-06-03 19:11:09 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.print("explor");
-        System.out.println(" exi");
+        System.out.print("Hello, world!");
     }
 }
