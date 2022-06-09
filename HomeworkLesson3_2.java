181bb3149d0ddc56aad495bd3f4e8e343bcd9846
mrGostem <106623766+mrGostem@users.noreply.github.com>
2022-06-09 16:00:46 +0300

    Update and rename Homework2Lesson3.java to HomeworkLesson3_2.java

--- HomeworkLesson3_2.java
+++ HomeworkLesson3_2.java
@@ -1,4 +1,4 @@
-public class Homework2Lesson3{
+public class HomeworkLesson3_2{
     static int[] numbers={0, 4, 5, 11, 23, 55, 155, 22, 90, 1223, -15, -27, -100};
     static int answer=0;
     public static void main(String[] args) {
