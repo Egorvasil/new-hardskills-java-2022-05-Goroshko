afc104b936aed970a19eb037125104be972c2873
mrGostem <106623766+mrGostem@users.noreply.github.com>
2022-06-09 16:01:48 +0300

    Update and rename Homework5Lesson3.java to HomeworkLesson3_5.java

--- HomeworkLesson3_5.java
+++ HomeworkLesson3_5.java
@@ -1,4 +1,4 @@
-public class Homework5Lesson3 {
+public class HomeworkLesson3_5 {
     public static void main(String[] args) {
         for(int x=0;x!=10;x++){
             for(int y=0;y<=x;y++) System.out.print("*");
