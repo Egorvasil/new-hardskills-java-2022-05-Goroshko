dc0715c37f8b87f40387273e6ad4d33b5f4c31de
mrGostem <106623766+mrGostem@users.noreply.github.com>
2022-06-09 16:00:16 +0300

    Update and rename Homework1Lesson3.java to HomeworkLesson3_1.java

--- HomeworkLesson3_1.java
+++ HomeworkLesson3_1.java
@@ -1,6 +1,6 @@
 import static java.lang.Math.sqrt;
 
-public class Homework1Lesson3{
+public class HomeworkLesson3_1{
     static int[] numbers={1, 3, 11, 2, 4, 102, 155, 171, 2990, 123, -2, -3, 18};
     public static void main(String[] args) {
         for(int i:numbers) if(i%2==0) System.out.println(i);
